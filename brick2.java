b14a416626d983b0f71352184d1f206fd45634e5
PrateekShrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2020-01-17 13:57:19 +0530

    Add files via upload

---
+++ brick2.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.*;
+public class brick2 {
+public static void main(String [] args)
+{
+    int b=0,d=0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number");
+    int a = sc.nextInt();
+    int c=a;
+    for(int i=1;i<=a;i++)
+    {
+        b=b+i;
+        d=d+i*2;
+        c=c-i-i*2;
+        if(b+d<a && c<i)
+        {
+            System.out.println("Patlu");
+            System.exit(0);
+        }
+    }
+    System.out.println("motu");
+}
+}
